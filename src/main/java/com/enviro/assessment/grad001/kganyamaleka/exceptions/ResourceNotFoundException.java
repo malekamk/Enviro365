547d67227d07ed693e4c698b0d6e368db2424df4
kmaleka <kganyamilton@gmail.com>
2025-01-13 11:44:12 +0200

    Feature: included exception package and custom exception

---
+++ src/main/java/com/enviro/assessment/grad001/kganyamaleka/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.enviro.assessment.grad001.kganyamaleka.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException(String message){
+        super(message);
+    }
+}
