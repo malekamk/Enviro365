91b4fbf20faa220cf30cc8cbc89a6813c682ff41
kmaleka <kganyamilton@gmail.com>
2025-01-15 09:40:31 +0200

    code review

--- src/main/java/com/enviro/assessment/grad001/kganyamaleka/config/CorsConfig.java
+++ src/main/java/com/enviro/assessment/grad001/kganyamaleka/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.enviro.assessment.grad001.kganyamaleka;
+package com.enviro.assessment.grad001.kganyamaleka.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
