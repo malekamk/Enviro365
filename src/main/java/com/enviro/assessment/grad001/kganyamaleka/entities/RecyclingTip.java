2f3fbfdec8366cd9e766be956aa2a4430e2ff874
kmaleka <kganyamilton@gmail.com>
2025-01-24 15:24:36 +0200

    updated readme

--- src/main/java/com/enviro/assessment/grad001/kganyamaleka/entities/RecyclingTip.java
+++ src/main/java/com/enviro/assessment/grad001/kganyamaleka/entities/RecyclingTip.java
@@ -3,7 +3,6 @@ package com.enviro.assessment.grad001.kganyamaleka.entities;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
